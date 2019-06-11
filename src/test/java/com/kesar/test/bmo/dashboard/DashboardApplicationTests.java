dd6be32d192026e16e67da5e1c97feaf91ac1a54
Kesar Singh <kesarai@yahoo.com>
2019-06-11 13:59:51 -0400

    Added API test cases

--- src/test/java/com/kesar/test/bmo/dashboard/DashboardApplicationTests.java
+++ src/test/java/com/kesar/test/bmo/dashboard/DashboardApplicationTests.java
@@ -1,16 +1,32 @@
 package com.kesar.test.bmo.dashboard;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import com.kesar.test.bmo.dashboard.service.PersonService;
+import com.kesar.test.bmo.dashboard.service.model.PersonDto;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class DashboardApplicationTests {
-
+	@Autowired
+	PersonService personService;
+	
 	@Test
-	public void contextLoads() {
+	public void testSearch() {
+		Page<PersonDto> p = personService.search("Jacelyn", PageRequest.of(0,10));
+		Assert.assertEquals("Jacelyn", p.getContent().get(0).getFirstName());
 	}
-
+	
+	@Test
+	public void testList() {
+		Page<PersonDto> p = personService.list(PageRequest.of(0,10));
+		Assert.assertEquals(10, p.getContent().size());
+	}
 }
